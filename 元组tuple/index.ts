74f6a6d6b4ca5eebd8d82cbc5cb47e59bc257952
wu-jp <13064157803@163.com>
2023-08-26 07:31:49 +0800

    元组特性和使用场景、symbol

--- 元组tuple/index.ts
+++ 元组tuple/index.ts
@@ -5,3 +5,10 @@ const b:Age = 123 // 正确
 // const a:Age = 'abc' // 报错
 
 type TupleEl = Tuple[number] // 联合类型 string|number|Date
+
+
+const arr = [1, 2] as const
+function add(x: number, y:number) {
+
+}
+add(...arr)
